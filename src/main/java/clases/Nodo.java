64db0a9d064ca1482c092c9b7b1aec6ae69708f4
JBJA <benjamin.soleado@gmail.com>
2022-03-10 14:02:48 -0600

    10/03/2022

---
+++ src/main/java/clases/Nodo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Juarez Arevalo
+ */
+public class Nodo {
+     private int dato;
+    private Nodo izquierda, derecha;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.izquierda = this.derecha = null;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
+
+    public void imprimirDato() {
+        System.out.println(this.getDato());
+    }
+}
